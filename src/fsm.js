12ead19f83ddaf073ed8388208049d59d317a309
Maksim Kirylka <maksimkirylka@gmail.com>
2017-09-23 22:27:51 +0300

    Implement FSM class with proposed API

--- src/fsm.js
+++ src/fsm.js
@@ -3,30 +3,60 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor(config = null) {
+        if (!config) {
+            throw new Error('config isn\'t passed');
+        }
+        this.history = [];
+        this.config = config;
+        this.states = Object.keys(this.config.states);
+        this.prevStateIndex = -1;
+        this.nextStateIndex = -1;
+        this.initialize();
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+        return this.state;
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+        if (this.states.indexOf(state) === -1) {
+            throw new Error('State isn\'t exist');
+        }
+        this.state = state;
+        const activeIndex = this.history.findIndex((item) => item.active);
+        this.updateActiveStateHistory();
+        this.history.push({ name: state, active: true });
+        this.prevStateIndex = activeIndex;
+    }
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger(event) {
+        const newState = this.config.states[this.getState()].transitions[event];
+        console.log('newState', event, this.config.states[this.getState()], newState);
+        if (!newState) {
+            throw new Error('There is no state');
+        }
+        this.changeState(newState);
+    }
 
     /**
      * Resets FSM state to initial.
      */
-    reset() {}
+    reset() {
+        this.changeState(this.config.initial);
+    }
 
     /**
      * Returns an array of states for which there are specified event transition rules.
@@ -34,26 +64,68 @@ class FSM {
      * @param event
      * @returns {Array}
      */
-    getStates(event) {}
+    getStates(event = null) {
+        if (!event) return this.states;
+        const states = [];
+        this.states.forEach((state) => {
+            const events = Object.keys(this.config.states[state].transitions);
+            if (events.indexOf(event) !== -1) {
+                states.push(state);
+            }
+        });
+        return states;
+    }
 
     /**
      * Goes back to previous state.
      * Returns false if undo is not available.
      * @returns {Boolean}
      */
-    undo() {}
+    undo() {
+        const size = this.history.length;
+        const activeIndex = this.history.findIndex((item) => item.active);
+        if (!size || activeIndex < 1) return false;
+        this.updateActiveStateHistory();
+        const prevIndex = this.prevStateIndex === -1 ? activeIndex - 1 : this.prevStateIndex;
+        this.state = this.history[prevIndex].name;
+        Object.assign(this.history[prevIndex], { active: true });
+        this.nextStateIndex = activeIndex;
+        this.prevStateIndex = -1;
+        return true;
+    }
 
     /**
      * Goes redo to state.
      * Returns false if redo is not available.
      * @returns {Boolean}
      */
-    redo() {}
+    redo() {
+        const size = this.history.length;
+        const activeIndex = this.history.findIndex((item) => item.active);
+        if (!size || activeIndex === size - 1) return false;
+        this.updateActiveStateHistory();
+        const nextIndex = this.nextStateIndex === -1 ? activeIndex + 1 : this.nextStateIndex;
+        this.state = this.history[nextIndex].name;
+        Object.assign(this.history[nextIndex], { active: true });
+        this.prevStateIndex = activeIndex;
+        this.nextStateIndex = -1;
+        return true;
+    }
 
     /**
      * Clears transition history
      */
-    clearHistory() {}
+    clearHistory() {
+        this.history = [];
+    }
+
+    initialize() {
+        this.reset();
+    }
+
+    updateActiveStateHistory() {
+        this.history = this.history.map(item => Object.assign(item, { active: false }));
+    }
 }
 
 module.exports = FSM;
